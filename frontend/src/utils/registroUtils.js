22ebc7888ac35441e945e716c4b1975b74aa8794
SevenShoes <sevenshoes.edocs@gmail.com>
2025-05-04 17:09:38 -0500

    fix: agregado archivo faltante registroUtils.js

---
+++ frontend/src/utils/registroUtils.js
@@ -0,0 +1,7 @@
+// frontend/src/utils/registroUtils.js
+import Swal from 'sweetalert2';
+import { v4 as uuidv4 } from 'uuid';
+
+export const handleRegistro = async () => {
+  // ... (todo el contenido de la función que usas)
+};
