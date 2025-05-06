3c547e8cb2216bba32d7d6a34b40aa66b8355c1e
SevenShoes <sevenshoes.edocs@gmail.com>
2025-05-06 16:56:03 -0500

    cambios git .bat

--- frontend/Subir Cambios a GitHub.bat
+++ frontend/Subir Cambios a GitHub.bat
@@ -1,22 +1,11 @@
 @echo off
-echo ===============================
+cd /d "%~dp0"
+
+echo ================================
 echo Subiendo cambios del FRONTEND...
-echo ===============================
-cd frontend
+echo ================================
 git add .
-git commit -m "🔒 Cambios de seguridad"
+git commit -m "Actualización general del frontend"
+git pull origin main --rebase
 git push origin main
-cd ..
-
-echo ===============================
-echo Subiendo cambios del BACKEND...
-echo ===============================
-cd backend
-git add -A
-git commit -m "🔒 Cambios de seguridad"
-git push origin main
-cd ..
-
-echo ===============================
-echo ✅ Cambios subidos con éxito
 pause
