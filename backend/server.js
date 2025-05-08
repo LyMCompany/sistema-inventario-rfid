63f715c69d8de2fa3cdc014ae34bb1a97e3217cd
SevenShoes <sevenshoes.edocs@gmail.com>
2025-05-07 22:40:37 -0500

    Fix CORS config and JSON.stringify for reportes

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,9 @@ const port = process.env.PORT || 5000;
 
 // ✅ Configuración CORS
 const corsOptions = {
-  origin: function (origin, callback) {
-    callback(null, true); // Acepta todas las URLs (ajusta esto en producción)
-  },
+  origin: [
+    'https://frontend-inventario-tmzb.onrender.com'
+  ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
